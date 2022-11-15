fdd00f10fbb17df5b501ab0cb1d5c5c4fbcc8dfd
lul1ya <luliamasfun1@gmail.com>
2022-11-15 14:47:54 +0100

    config

---
+++ src/main/java/com/group8/bookingapp/BookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.group8.bookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppApplication.class, args);
+    }
+
+}
