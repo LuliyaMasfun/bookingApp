a15fd7e3b76c9b4d70a534aa399bee3a88fd0ea7
lul1ya <luliamasfun1@gmail.com>
2022-11-17 13:39:25 +0100

    Update BookingAppApplication.java

--- src/main/java/com/group8/bookingapp/BookingAppApplication.java
+++ src/main/java/com/group8/bookingapp/BookingAppApplication.java
@@ -8,7 +8,6 @@ public class BookingAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookingAppApplication.class, args);
-        System.out.println();
     }
 
 
