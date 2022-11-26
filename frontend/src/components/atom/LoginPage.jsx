df5926df4b6023e2fe0125417e462b3164359a80
YaffetK <yaffet97@gmail.com>
2022-11-27 00:51:46 +0100

    WIP frontend

--- frontend/src/components/atom/LoginPage.jsx
+++ frontend/src/components/atom/LoginPage.jsx
@@ -6,65 +6,65 @@ import "../../css/style.css";
 
 
 const LoginPage = () => {
+
     return (
 
-        <>
-            <html>
-                <body>
-                    <div class="layout">
-                        <div class="welcomeScreen">
-                            <p id="companyName">We make it BIG.</p>
-                        </div>
+        <div className="container">
+            <div className="input_container">
+
+                <div className="layout">
+                    <div className="welcomeScreen">
+                        <p id="companyName">We make it BIG.</p>
                     </div>
-                    <div class="whiteBackground" id="whiteBackground">
-                        <div class="formBox" id="formBox">
-                            <p class="accountExits">
-                                Have an account? <span class="textColor">Sign In</span>
-                            </p>
+                </div >
+                <div className="whiteBackground" id="whiteBackground" >
+                    <div className="formBox" id="formBox" >
+                        <p className="accountExits" >
+                            Have an account ? <span className="textColor" > Sign In</span >
+                        </p >
 
-                            <p class="welcomeText" id="welcomeText">Welcome to BIG</p>
+                        <p className="welcomeText" id="welcomeText" > Welcome to BIG</p >
 
-                            <p class="smallTextUnderWelcomeText">
-                                Blandit libero volutpat sed cras ornare arcu dui. Accumsan in ni nisi
-                                scelerisque eu.
-                            </p>
+                        <p className="smallTextUnderWelcomeText" >
+                            Blandit libero volutpat sed cras ornare arcu dui.Accumsan in ni nisi
+                            scelerisque eu.
+                        </p >
 
-                            <div class="formINput" id="formInput">
-                                <p class="smallTextAbove1">Full name</p>
+                        <div className="formINput" id="formInput" >
+                            <p className="smallTextAbove1" > Full name</p >
 
-                                <input
-                                    class="fullNameForm"
-                                    type="text"
-                                    placeholder="Username"
-                                    id="formFullName"
-                                    onkeydown="validateFullName()"
-                                />
-                                <p class="smallTextAbove2">E-mail</p>
-                                <input
-                                    class="fullNameForm"
-                                    type="text"
-                                    placeholder="example@gmail.com"
-                                    id="formEmail"
-                                    onkeydown="validateEmail()"
-                                />
-                                <p class="smallTextAbove3">Password</p>
-                                <input
-                                    class="fullNameForm"
-                                    type="password"
-                                    placeholder="8+ strong character"
-                                    id="formPassword"
-                                    onkeydown="validatePassword()"
-                                />
-                                <button class="submitButton" type="button" onclick="register()">
-                                    Create an account
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <script src="/src/js/validateLogIn.js"></script>
-                </body>
-            </html>
-        </>
+                            <input
+                                className="fullNameForm"
+                                type="text"
+                                placeholder="Username"
+                                id="formFullName"
+                                onkeydown="validateFullName()"
+                            />
+                            <p className="smallTextAbove2" > E - mail</p >
+                            <input
+                                className="fullNameForm"
+                                type="text"
+                                placeholder="example@gmail.com"
+                                id="formEmail"
+                                onkeydown="validateEmail()"
+                            />
+                            <p className="smallTextAbove3" > Password</p >
+                            <input
+                                className="fullNameForm"
+                                type="password"
+                                placeholder="8+ strong character"
+                                id="formPassword"
+                                onkeydown="validatePassword()"
+                            />
+                            <button className="submitButton" type="button" onclick="register()" >
+                                Create an account
+                            </button >
+                        </div >
+                    </div >
+                </div >
+
+            </div>
+        </div >
     )
 }
 export default LoginPage
