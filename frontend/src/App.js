cf18bb794b475ff14a9eb28681a3ed8b0720cabf
YaffetK <yaffet97@gmail.com>
2022-12-01 13:24:20 +0100

    Design for signin page and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
-import Calendar from "react-calendar";
-import SignIn from "./components/molecule/SignIn";
-
+import Register from "./components/molecule/Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LoginPage from "./components/molecule/LoginPage";
 function App() {
   return (
-    <div className="App">
-      <Calendar />
-      {/* <SignIn /> */}
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/register"> Register</Link>
+        <Link to="/about"> About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/molecule/LoginPage.jsx
+++ frontend/src/components/molecule/LoginPage.jsx
@@ -1,186 +1,52 @@
 import React, { useState } from "react";
-import "../../css/layout.css"
-import "../../css/animation.css";
-import "../../css/formBox.css";
 import "../../css/style.css";
-import SignIn from "./SignIn";
-
 
 const LoginPage = () => {
 
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setpassword] = useState("")
 
-    const validateEmail = () => {
-        const inputFullname = document.getElementById("formEmail").value;
-        const inputFullNameWithoutValue = document.getElementById("formEmail");
-        const pattern =
-            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (inputFullname.match(pattern)) {
-            const findInvalidImg = document.getElementById("invalidEmailImg");
-            const findvalidImg = document.getElementById("validEmailImg");
-            if (findInvalidImg != null) {
-                findInvalidImg.remove();
-            }
-            if (findvalidImg != null) {
-                findvalidImg.remove();
-            }
-        }
-    }
 
-    const validatePassword = () => {
-        const inputFullname = document.getElementById("formPassword").value;
-        const inputFullNameWithoutValue = document.getElementById("formPassword");
-        const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/;
-        if (inputFullname.match(pattern)) {
-            const findInvalidImg = document.getElementById("invalidPasswordImg");
-            const findvalidImg = document.getElementById("validPasswordImg");
-            if (findInvalidImg != null) {
-                findInvalidImg.remove();
-            }
-            if (findvalidImg != null) {
-                findvalidImg.remove();
-            }
 
-            const createValidImg = document.createElement("img");
-            createValidImg.src = "/src/main/resources/static/images/valid.png";
-            createValidImg.setAttribute("class", "validPasswordImg");
-            createValidImg.setAttribute("id", "validPasswordImg");
-            const findDiv = document.getElementById("formBox");
-            findDiv.appendChild(createValidImg);
-        } else if (inputFullname == "") {
-            const findInvalidImg = document.getElementById("invalidPasswordImg");
-            const findvalidImg = document.getElementById("validPasswordImg");
-            if (findInvalidImg != null || findvalidImg != null) {
-                findInvalidImg.remove();
-                findvalidImg.remove();
-            }
-        } else {
-            const findvalidImg = document.getElementById("validPasswordImg");
-            const findInvalidImg = document.getElementById("invalidPasswordImg");
-            if (findInvalidImg != null) {
-                findInvalidImg.remove();
-            }
-            if (findvalidImg != null) {
-                findvalidImg.remove();
-            }
 
-            const createValidImg = document.createElement("img");
-            createValidImg.src = "/src/main/resources/static/images/invalid.png";
-            createValidImg.setAttribute("class", "invalidPasswordImg");
-            createValidImg.setAttribute("id", "invalidPasswordImg");
-            const findDiv = document.getElementById("formBox");
-            findDiv.appendChild(createValidImg);
-        }
-    }
-
-    const validateFullName = () => {
-        const inputFullname = document.getElementById("formFullName").value;
-        const inputFullNameWithoutValue = document.getElementById("formFullName");
-        const pattern = /^[a-zA-Z\s]+$/;
-        if (inputFullname.match(pattern)) {
-            const findInvalidImg = document.getElementById("invalidNameImg");
-            const findvalidImg = document.getElementById("validNameImg");
-            if (findInvalidImg != null) {
-                findInvalidImg.remove();
-            }
-            if (findvalidImg != null) {
-                findvalidImg.remove();
-            }
-
-            const createValidImg = document.createElement("img");
-            createValidImg.src = "/src/main/resources/static/images/valid.png";
-            createValidImg.setAttribute("class", "validNameImg");
-            createValidImg.setAttribute("id", "validNameImg");
-            const findDiv = document.getElementById("formBox");
-            findDiv.appendChild(createValidImg);
-        } else if (inputFullname == "") {
-            const findInvalidImg = document.getElementById("invalidNameImg");
-            const findvalidImg = document.getElementById("validNameImg");
-            if (findInvalidImg != null || findvalidImg != null) {
-                findInvalidImg.remove();
-                findvalidImg.remove();
-            }
-        } else {
-            const findvalidImg = document.getElementById("validNameImg");
-            const findInvalidImg = document.getElementById("invalidNameImg");
-            if (findInvalidImg != null) {
-                findInvalidImg.remove();
-            }
-            if (findvalidImg != null) {
-                findvalidImg.remove();
-            }
-
-            const createValidImg = document.createElement("img");
-            createValidImg.src = "/src/main/resources/static/images/invalid.png";
-            createValidImg.setAttribute("class", "invalidNameImg");
-            createValidImg.setAttribute("id", "invalidNameImg");
-            const findDiv = document.getElementById("formBox");
-            findDiv.appendChild(createValidImg);
-        }
-    }
 
     return (
 
-        <div className="container">
-            <div className="input_container">
+        <div className='site-wrapper'>
+            <div className='login-container'>
+                <h1>
+                    Logga in
+                </h1>
+                <div className='login-form-container'>
+                    <form>
+                        <div className='login-form-wrapper'>
+                            <div className='input-holder'>
+                                <label htmlFor="email">Email</label>
+                                <input id="email" type='text' placeholder='JohnDoe@gmail.com'></input>
+                            </div>
+                            <div className='input-holder'>
+                                <label htmlFor="password">Password</label>
+                                <input id="password" type='text' placeholder='JohnDoe@gmail.com'></input>
+                            </div>
+                        </div>
+                        {/* <div className='register-btn-container'>
+                            <button className='text-btn'>Register</button>
+                            <button className='text-btn'>Forgot password?</button>
+                        </div> */}
+                        <button type='submit' className='primary-btn'>
+                            Log In
+                        </button>
 
-                <div className="layout">
-                    <div className="welcomeScreen">
-                        <p id="companyName">We make it BIG.</p>
-                    </div>
-                </div >
-                <div className="whiteBackground" id="whiteBackground" >
-                    <div className="formBox" id="formBox" >
-                        <p className="accountExits" >
-                            Have an account ? <span className="textColor" > Sign In</span >
-                        </p >
-
-                        <p className="welcomeText" id="welcomeText" > Welcome to BIG</p >
-
-                        <p className="smallTextUnderWelcomeText" >
-                            Blandit libero volutpat sed cras ornare arcu dui.Accumsan in ni nisi
-                            scelerisque eu.
-                        </p >
-
-                        <div className="formINput" id="formInput" >
-                            <p className="smallTextAbove1" > Full name</p >
-
-                            <input
-                                className="fullNameForm"
-                                type="text"
-                                placeholder="Username"
-                                id="formFullName"
-                                onKeyDown={validateFullName}
-                            />
-                            <p className="smallTextAbove2" > E - mail</p >
-                            <input
-                                className="fullNameForm"
-                                type="text"
-                                placeholder="example@gmail.com"
-                                id="formEmail"
-                                onKeyDown={validateEmail}
-                            />
-                            <p className="smallTextAbove3" > Password</p >
-                            <input
-                                className="fullNameForm"
-                                type="password"
-                                placeholder="8+ strong character"
-                                id="formPassword"
-                                onKeyDown={validatePassword}
-                            />
-                            <button className="submitButton" type="button" onClick="register()" >
-                                Create an account
-                            </button >
-                        </div >
-                    </div >
-                </div >
-
-            </div>
-
-            <SignIn />
+                        <div className="message">
+                            <p>Don´t have an account?<br />
+                                <a href="/register"> Register here</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div >
         </div >
+
     )
 }
 export default LoginPage
