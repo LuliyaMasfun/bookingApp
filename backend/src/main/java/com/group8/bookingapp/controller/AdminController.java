8ed683a70e4893d3c4e8e8948f174e7a9b04a5ed
denisecasp <den.casperson@gmail.com>
2022-12-02 13:43:41 +0100

    Added Get and Post for AdminController

--- backend/src/main/java/com/group8/bookingapp/controller/AdminController.java
+++ backend/src/main/java/com/group8/bookingapp/controller/AdminController.java
@@ -1,11 +1,28 @@
 package com.group8.bookingapp.controller;
 
-import com.group8.bookingapp.repository.UserRepo;
+import com.group8.bookingapp.models.Camera;
+import com.group8.bookingapp.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 public class AdminController {
+
+    @Autowired
+    private CameraRepo cameraRepo;
+
+
+    @GetMapping(value = "/allCamera")
+    public List<Camera> getAllCameras(){
+        return cameraRepo.findAll();
+    }
+
+    @PostMapping(value = "/addCamera")
+    public List<Camera> addCamera(@RequestBody final Camera camera){
+        return cameraRepo.addCamera(camera);
+    }
 
 }
