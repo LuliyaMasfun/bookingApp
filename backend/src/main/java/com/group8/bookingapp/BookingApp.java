0d67cac44d65701ccd00bcbe537043a89c89b2db
Sh1re-a <83682762+Sh1re-a@users.noreply.github.com>
2022-12-06 01:16:07 +0100

    Nu går det o köra, ändrat till Java 16

--- backend/src/main/java/com/group8/bookingapp/BookingApp.java
+++ backend/src/main/java/com/group8/bookingapp/BookingApp.java
@@ -2,9 +2,15 @@ package com.group8.bookingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
+@EnableSwagger2
 @SpringBootApplication
 public class BookingApp {
 
@@ -12,4 +18,12 @@ public class BookingApp {
         SpringApplication.run(BookingApp.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
